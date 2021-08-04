e2c87900aa06628aa04bf6eb03a6fe670e726d71
ksheeraj kandra <kandra.ksheeraj@gmail.com>
2021-08-04 15:14:56 +0530

    Create Subplots.R

---
+++ Subplots.R
@@ -0,0 +1,6 @@
+t = 0:pi/10:2*pi;
+[X,Y,Z] = cylinder(4*cos(t));
+subplot(2,2,1); mesh(X); title('X');
+subplot(2,2,2); mesh(Y); title('Y');
+subplot(2,2,3); mesh(Z); title('Z');
+subplot(2,2,4); mesh(X,Y,Z); title('X,Y,Z');
